fc3e5ab3619c2ce4b57c218c3a47dcff9efbf949
root <ccschori@gmail.com>
2022-09-08 14:35:22 -0700

    My first C hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/
+/**
 *main -Entry point
-*return: 0 after printing the function
+*Return: 0 after printing the function
 int main (void)
 {
-puts ('\'"Programming is like building a multilingual puzzle");
+puts ("\"Programming is like building a multilingual puzzle");
 Return (0);
 }
