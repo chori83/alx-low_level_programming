19151fade30f507aac0bb4e6a4f00f27a40b5f7f
root <ccschori@gmail.com>
2022-09-08 15:58:19 -0700

    My first C hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -prints to string
+ * Description:prints "and that piece of art is useful --" without puts
+ * Return :1
+ */
+int main(void)
+{
+	char *s="and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",
+        long l=59;
+	long fd=1
+	longsyscall=1;
+	long ret=0;
+	__asm__ ("syscall"
+	: "=a" (ret)
+	:"a" (syscall),
+	"D" (fd),
+	"S" (s)
+	"d" (1)
+	return (1);
+}
