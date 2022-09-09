be0ae88966161152a36e04f64b84d07baea6a207
root <ccschori@gmail.com>
2022-09-09 09:52:07 -0700

    My first C if else task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,6 @@
  * File: 0-positive_or_negative.c
  * Auth: chori
  */
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -15,18 +14,15 @@
 */
 int main(void)
 {
-    int n;
-
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-
-    if (n > 0)
-        printf("%d is positive\n", n);
-    else if (n < 0)
-        printf("%d is negative\n", n);
-    else
-        printf("%d is zero\n", n);
-
-    return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+printf("%d is zero\n", n);
+return (0);
 }
 
