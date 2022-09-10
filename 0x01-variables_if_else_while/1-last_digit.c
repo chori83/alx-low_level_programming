60c78dfad17032789f9172c21bb7c854158ae5cc
root <ccschori@gmail.com>
2022-09-10 15:15:00 -0700

    My first C if else task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,8 @@ if ((n % 10) > 5)
 n, n % 10);
 }
 else if ((n % 10) < 6 && (n % 10) != 0)
-{				       printf("Last digit of %d is %d and is less than 6 and not 0\n",	
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n",	
 n, n % 10);
 }
 else
