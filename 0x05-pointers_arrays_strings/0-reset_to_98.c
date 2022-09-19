bff5fd5fb2e0b3553cc70e912f8ac46e31109a6b
root <ccschori@gmail.com>
2022-09-19 10:19:06 -0700

    My first C pointers task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- *  * reset_to_98 - updates the value of n to 98
- *   * @n: integer pointer to a variable to be updated
- *    *
- *     * Return: nothing
- *      */
+ * reset_to_98 - updates the value of n to 98
+ * @n: integer pointer to a variable to be updated
+ *
+ * Return: nothing
+ *
+ */
 
 void reset_to_98(int *n)
 {
